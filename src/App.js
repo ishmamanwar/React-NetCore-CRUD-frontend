3ec7cf9a3132f73ec952a694ef03cd91f6144530
ishmamanwar <ishmam.anwar96@gmail.com>
2021-04-23 20:21:35 -0600

    Product page created and working

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
     <div className="container">
       <h3 className="m-3 d-flex justify-content-center">
-        React JS Experiment
+        React .Net Core CRUD
       </h3>
     
     <Navigation/>
