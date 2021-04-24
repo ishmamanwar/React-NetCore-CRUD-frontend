ffe1610cc8e71ebd53fc57b03dc57df67147e809
ishmamanwar <ishmam.anwar96@gmail.com>
2021-04-23 19:04:47 -0600

    EditCategory Modal created

--- src/EditCatModal.js
+++ src/EditCatModal.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Modal,Button, Row, Col, Form} from 'react-bootstrap';
 
-export class AddCatModal extends Component{
+export class EditCatModal extends Component{
     constructor(props){
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -10,12 +10,13 @@ export class AddCatModal extends Component{
     handleSubmit(event){
         event.preventDefault();
         fetch(process.env.REACT_APP_API+'category', {
-            method:'POST',
+            method:'PUT',
             headers:{
                 'Accept':'application/json',
                 'Content-Type':'application/json'
             },
             body: JSON.stringify({
+                CategoryId: event.target.CategoryId.value,
                 CategoryName: event.target.CategoryName.value,
                 Active: event.target.Active.value,
                 CategoryDescription: event.target.CategoryDescription.value
@@ -41,23 +42,31 @@ export class AddCatModal extends Component{
                 >
                     <Modal.Header closeButton>
                         <Modal.Title id="contained-modal-title-vcenter">
-                            Add Category
+                            Edit Category
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <Row>
                             <Col sm={6}>
                                 <Form onSubmit={this.handleSubmit}>
+                                <Form.Group controlId="CategoryId">
+                                        <Form.Label>CategoryId</Form.Label>
+                                        <Form.Control type="text" name="CategoryId" required
+                                        disabled
+                                        defaultValue={this.props.catid}
+                                        placeholder="CategoryId" />
+                                    </Form.Group>
                                     <Form.Group controlId="CategoryName">
                                         <Form.Label>CategoryName</Form.Label>
                                         <Form.Control type="text" name="CategoryName" required
+                                        defaultValue={this.props.catname}
                                         placeholder="CategoryName" />
                                     </Form.Group>
                                     <Form.Group controlId="Active">
                                         <Form.Label>Active</Form.Label>
                                         {/*<Form.Control type="text" name="Active" required
                                         placeholder="Active" />*/}
-                                        <Form.Control name="Active" required as="select">
+                                        <Form.Control name="Active" required as="select" required defaultValue={this.props.active}>
                                             <option value="1">Active</option>
                                             <option value="0">Inactive</option>
                                         </Form.Control>
@@ -65,11 +74,12 @@ export class AddCatModal extends Component{
                                     <Form.Group controlId="CategoryDescription">
                                         <Form.Label>CategoryDescription</Form.Label>
                                         <Form.Control type="text" name="CategoryDescription" required
+                                        defaultValue={this.props.catdesc}
                                         placeholder="CategoryDescription" />
                                     </Form.Group>
                                     <Form.Group>
                                         <Button variant="primary" type="submit">
-                                            Add Category
+                                            Update Category
                                         </Button>
                                     </Form.Group>
                                 </Form>
